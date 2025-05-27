a2ec34b6d34f6ab5f2bd5e9992a387f7f67c64d0
Moisés Jorge <moisesjorge30@gmail.com>
2025-05-28 00:29:39 +0100

    criação do projecto

---
+++ appy.py
@@ -0,0 +1,14 @@
+# Passos que devem ser seguidos para trabalhar com o tkinter
+# 1 - Configuração da Aparência
+
+
+# 2 - Criação da janela principal
+
+
+# 3 - Criação dos campos necessários
+
+
+# 4 - Criação das funções/funcionalidades
+
+
+# 5 - Iniciar a aplicação
